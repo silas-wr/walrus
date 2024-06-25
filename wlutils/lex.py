eed31afbe6b4dcb65ef27f6e747a088253e44eaf
Silas Wrinker <alcosojr79@gmail.com>
2024-06-25 15:56:00 -0400

    Update lex.py

--- wlutils/lex.py
+++ wlutils/lex.py
@@ -5,10 +5,10 @@ import wlutils.err.*
 # try to open the conf.w file
 
 try:
-  file = open("conf.w", "r")
+    file = open("conf.w", "r")
 
 except:
-  raise ConfigFileNotFound("conf.w not found in this directory")
+    raise ConfigFileNotFound("conf.w not found in this directory")
 
 # found conf.w
 
