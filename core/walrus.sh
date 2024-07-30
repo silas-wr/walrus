56385c439539e2362a00df3868cc875a28e33f6c
Silas Wrinker <alcosojr79@gmail.com>
2024-07-30 16:39:28 -0400

    Update walrus.sh

--- core/walrus.sh
+++ core/walrus.sh
@@ -16,6 +16,8 @@ then
   printf "2) See Walrus status for this directory\n"
   printf "3) Learn how to use Walrus\n\n"
 
+  read -n 1 -p "Select one option: " opt
+
 else
   if [ "$1" = "-i" ];
   then
