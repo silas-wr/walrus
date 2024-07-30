9ba732b7718b2fd4a913501c42cf2e4be275fec0
Silas Wrinker <alcosojr79@gmail.com>
2024-07-30 15:48:46 -0400

    Update walrus.sh

--- core/walrus.sh
+++ core/walrus.sh
@@ -6,4 +6,10 @@ echo -e "\033[31;1m@\033[0;1m  \033[31;1m@@@@@@\033[0;1m   /  _  / /_ / /   /_ /
 echo -e "\033[31;1m@\033[0;1m  \033[31;1m@@@@@@\033[0;1m  /_/ /_/ /  / /__ /  ( /__/ __/ (_) __/ /  /  \033[0;0m\n"
 echo -e "\033[0;1m--------------------------------------------------------\033[0;0m\n"
 
-echo "Welcome to the Walrus logging platform! What would you like to do today?\n"
+echo -e "Welcome to the Walrus logging platform! What would you like to do today?\n"
+
+echo "1) Create new Walrus project"
+echo "2) See Walrus status for this directory"
+echo -e "3) Learn how to use Walrus\n"
+
+read -n -1 -p "Select 1, 2, or 3: " option
