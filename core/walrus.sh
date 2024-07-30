a52f03f8d101bb753a6af548140aeb56db248f93
Silas Wrinker <alcosojr79@gmail.com>
2024-07-30 16:05:42 -0400

    Update walrus.sh

--- core/walrus.sh
+++ core/walrus.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-printf "\n\033[0;1m--------------------------------------------------------\033[0;0m"
-printf "\033[0;1m             _    _   __       _         _       _  /  /\033[0;0m"
-printf "\033[31;1m@\033[0;1m  \033[31;1m@@@@@@\033[0;1m   /  _  / /_ / /   /_ / /  / /_  _   /_  /--/ \033[0;0m"
-printf "\033[31;1m@\033[0;1m  \033[31;1m@@@@@@\033[0;1m  /_/ /_/ /  / /__ /  ( /__/ __/ (_) __/ /  /  \033[0;0m\n"
-printf "\033[0;1m--------------------------------------------------------\033[0;0m\n"
+printf "\n\033[0;1m--------------------------------------------------------\033[0;0m\n"
+printf "\033[0;1m             _    _   __       _         _       _  /  /\033[0;0m\n"
+printf "\033[31;1m@\033[0;1m  \033[31;1m@@@@@@\033[0;1m   /  _  / /_ / /   /_ / /  / /_  _   /_  /--/ \033[0;0m\n"
+printf "\033[31;1m@\033[0;1m  \033[31;1m@@@@@@\033[0;1m  /_/ /_/ /  / /__ /  ( /__/ __/ (_) __/ /  /  \033[0;0m\n\n"
+printf "\033[0;1m--------------------------------------------------------\033[0;0m\n\n"
 
-printf "Welcome to the Walrus logging platform!" 
+printf "Welcome to the Walrus logging platform!\n" 
 
-if [[ -z $1 ]];
+if [ -z $1 ];
 then
-  printf "What would you like to do today?\n"
+  printf "What would you like to do today?\n\n"
 
-  printf "1) Create new Walrus project"
-  printf "2) See Walrus status for this directory"
-  printf "3) Learn how to use Walrus\n"
+  printf "1) Create new Walrus project\n"
+  printf "2) See Walrus status for this directory\n"
+  printf "3) Learn how to use Walrus\n\n"
 
 else
-  if [ $1 == "-i" ];
+  if [ $1 = "-i" ];
   then
-    printf "[i] input overridden. test succeeded."
+    printf "[i] input overridden. test succeeded.\n"
 
   else
-    printf "[!] unknown option $1"
+    printf "[!] unknown option $1\n"
 
   fi
 
